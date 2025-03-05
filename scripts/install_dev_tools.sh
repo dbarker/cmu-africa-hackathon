296ea2efc5f4b2f68622bacc55a4192ff1ac40ce
Douglas Barker <doug.barker@energid.com>
2025-03-05 09:21:21 -0700

    add git attributes file to preserve linux line endings on shell scripts.

--- scripts/install_dev_tools.sh
+++ scripts/install_dev_tools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update
-apt-get install -y nano gitk cmake-gui libtool pkg-config gdb gdbserver wget libgmock-dev python3 python3-pip python3-venv clang-format clang-tidy clang llvm llvm-dev libclang-dev yamllint
+apt-get install -y nano gitk cmake-gui libtool pkg-config gdb gdbserver wget libgmock-dev python3 python3-pip python3-venv clang-format
